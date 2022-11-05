fdfc79d8d6f72e2ecc62269594fe1adf63725162
Diogo Carvalho <1200611@isep.ipp.pt>
2022-11-05 02:26:17 +0000

    US 2/3/4

--- dddnetcore/Domain/Warehouse/WarehouseAdress.cs
+++ dddnetcore/Domain/Warehouse/WarehouseAdress.cs
@@ -4,7 +4,34 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DDDSample1.Domain.Warehouse
 {
-[ComplextType]
+    [ComplextType]
+    public class WarehouseAdress : IValueObject
+    {
+        public string street {get;private set;}
+        public string country {get;private set;}
+        public int doorNumber {get;private set;}
+
+        public WarehouseAdress()
+        {
+            
+        }
+
+        public WarehouseAdress(string street, string country, int doorNumber)
+        {
+            this.updateWarehouseAdress(street,country,doorNumber);
+        }
+
+        public void updateWarehouseAdress(string street,string country, int doorNumber)
+        {
+            try{
+                this.street= street;
+                this.country= country;
+                this.doorNumber=doorNumber;
+            }catch{
+                throw new BusinessRuleValidationException("Invalid Adress");
+            }
+        }
+    }
 }
 
 
