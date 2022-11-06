77591b6bb8049a3df9d95c769b4041921ac74b44
1190772 <1190772@isep.ipp.pt>
2022-11-06 02:50:29 +0000

    DeliveryEntityTypeConfiguration

--- dddnetcore/Infraestructure/Delivery/DeliveryEntityTypeConfiguration.cs
+++ dddnetcore/Infraestructure/Delivery/DeliveryEntityTypeConfiguration.cs
@@ -8,25 +8,15 @@ namespace DDDSample1.Infrastructure.Delivery
     {
         public void Configure(EntityTypeBuilder<Delivery> builder)
         {
-            /*
+            
             builder.HasKey(n => n.Id);
-            builder.OwnsOne(u => u.Nome);
-            builder.OwnsOne(u => u.Email);
-            builder.OwnsOne(u => u.Password);
-            builder.OwnsOne(u => u.DescBreve);
-            builder.OwnsOne(u => u.Residencia);
-            builder.OwnsOne(u => u.DataNascimento);
-            builder.OwnsMany(u => u.Tags);
-            builder.OwnsOne(u => u.Telefone);
-            builder.OwnsOne(u => u.Avatar);
-            builder.OwnsOne(u => u.PerfilLinkedin);
-            builder.OwnsOne(u => u.PerfilFacebook);
-            builder.OwnsOne(u => u.Estado).Property(e => e.DataEstadoEmocional);
-            builder.OwnsOne(u => u.Estado).Property(emocional => emocional.Estado)
-                .HasConversion(c => c.ToString(),
-                    s => Enum.Parse<EstadoHumor>(s));
-            builder.OwnsOne(u => u.Estado).Property(e => e.Val);
-            */
+            builder.OwnsOne(u => u.DeliveryDate);
+            builder.OwnsOne(u => u.DeliveryWarehouseID);
+            builder.OwnsOne(u => u.DeliveryWeight);
+            builder.OwnsOne(u => u.DeliveryInTime);
+            builder.OwnsOne(u => u.DeliveryOutTime);
+            
+    
         }
     }
 }
