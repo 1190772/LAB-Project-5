8f3f14810b051a7f12613bb48034b2bd5fb74c5c
1190772 <1190772@isep.ipp.pt>
2022-11-06 03:42:56 +0000

    DeliveryMapper

--- dddnetcore/Domain/Deliveries/DeliveryMapper.cs
+++ dddnetcore/Domain/Deliveries/DeliveryMapper.cs
@@ -1,7 +1,23 @@
+namespace DDDSample1
+{
+
+    public class DeliveryMapper
+    {
+
+        public DeliveryDTO toDTO(Delivery domain)
+        {
+            return new DeliveryDTO(domain.id, domain.DateOfDelivery,
+             domain.Weight, domain.WarehouseID, domain.PutDeliveryTime, domain.TakeDeliveryTime);
+        }
+
+        public Delivery toDomain(DeliveryDTO dto)
+        {
+            return new Delivery(dto.id, dto.DeliveryDate, dto.Weight, dto.WarehouseID, dto.PutDeliveryTime, dto.TakeDeliveryTime);
+        }
+
+    }
 
 
+}
 
-/*public classs Mapper{
 
-    public DeliveryDTO toDTO
-}*/
