8a72b0e81e4a5eeac99730f02d3e62f859ddec58
Diogo Carvalho <1200611@isep.ipp.pt>
2022-11-28 17:24:33 +0000

    Create Trucks sending data to dummy database

--- spa/src/app/create-truck/create-truck.component.ts
+++ spa/src/app/create-truck/create-truck.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-create-truck',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTruckComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+  onTruckCreate(trucks:{tLicencePlate:string,tName:string,tTare:string,tMaximumLoad:string,tBateryCapacity:string,tAutonomy:string,tChargingTime:string}) {
+    console.log(trucks);
+    this.http.post('https://lapr5dummydb-default-rtdb.europe-west1.firebasedatabase.app/trucks.json',trucks)
+      .subscribe((res)=>{
+        console.log(res);
+      })
   }
 
 }
