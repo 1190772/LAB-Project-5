975173b4318e2c1e013da358b2dfd77827e0721d
Diogo Carvalho <1200611@isep.ipp.pt>
2022-11-28 00:48:23 +0000

    tried to revert last 2 commits

---
+++ spa/src/app/Warehouse.ts
@@ -0,0 +1,22 @@
+export interface Warehouse{
+  id: string;
+  adress: string;
+  description:string;
+}
+export const warehouses=[
+  {
+    id:"1",
+    adress:"Rua numero 1",
+    description:"Warehouse numero 1"
+  },
+  {
+    id:"2",
+    adress:"Rua numero 2",
+    description:"Warehouse numero 2"
+  },
+  {
+    id:"3",
+    adress:"Rua numero 3",
+    description:"Warehouse numero 3"
+  }
+]
