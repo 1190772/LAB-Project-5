c5a1500e8ead4b409f358fd034bb7ac76a9588a0
1190772 <1190772@isep.ipp.pt>
2022-12-02 18:25:05 +0000

    Fixes

--- spa/src/app/road-network/road-network.component.ts
+++ spa/src/app/road-network/road-network.component.ts
@@ -27,6 +27,7 @@ export class RoadNetworkComponent implements OnInit, AfterViewInit {
     return this.canvasRef?.nativeElement;
   }
 
+
   private loader = new THREE.TextureLoader();
   private geometry = new THREE.BoxGeometry(1,1,1);
   private material = new THREE.MeshBasicMaterial({map: this.loader.load(this.texture)});
@@ -40,7 +41,38 @@ export class RoadNetworkComponent implements OnInit, AfterViewInit {
   private createScene() {
 
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x00FF00);
+    this.scene.background = new THREE.Color(0xFFFFFF);
+
+    let gData= {
+      nodes: [
+        { "id": 1, "name": "Arouca", "lat": -50.0000, "lon": -42.6618, "alt": 15.6250, "links": [6, 13] },                          // 0
+        { "id": 2, "name": "Espinho", "lat": 41.0072, "lon": 8.6410, "alt": 550, "links": [4, 9, 16] },                      // 1
+        { "id": 3, "name": "Gondomar", "lat": 42.1115, "lon": 8.7613, "alt": 200, "links": [3, 8, 10, 12] },                 // 2
+        { "id": 4, "name": "Maia", "lat": 41.2279, "lon": 8.6210, "alt": 700, "links": [4, 7, 12, 15, 16] },                 // 3
+        { "id": 5, "name": "Matosinhos", "lat": 41.1844, "lon": 8.6963, "alt": 350, "links": [15] },                         // 4
+        { "id": 6, "name": "Oliveira de Azeméis", "lat": 40.8387, "lon": 8.4770, "alt": 750, "links": [9, 11, 13] },         // 5
+        { "id": 7, "name": "Paredes", "lat": 41.2052, "lon": 8.3304, "alt": 0, "links": [10, 13, 14] },                      // 6
+        { "id": 8, "name": "Porto", "lat": 41.1579, "lon": 8.6291, "alt": 600, "links": [16] },                              // 7
+        { "id": 9, "name": "Póvoa de Varzim", "lat": 41.3804, "lon": 8.7609, "alt": 400, "links": [15] },                    // 8
+        { "id": 10, "name": "Santa Maria da Feira", "lat": 40.9268, "lon": 8.5483, "alt": 100, "links": [11, 12, 14, 16] },  // 9
+        { "id": 11, "name": "Santo Tirso", "lat": 41.3431, "lon": 8.4738, "alt": 650, "links": [12, 14] },                   // 10
+        { "id": 12, "name": "São João da Madeira", "lat": 40.9005, "lon": 8.4907, "alt": 300, "links": [13, 14] },           // 11
+        { "id": 13, "name": "Trofa", "lat": 41.3391, "lon": 8.5600, "alt": 450, "links": [14] },                             // 12
+        { "id": 14, "name": "Vale de Cambra", "lat": 40.8430, "lon": 8.3956, "alt": 50, "links": [] },                       // 13
+        { "id": 15, "name": "Valongo", "lat": 41.1887, "lon": 8.4983, "alt": 800, "links": [] },                             // 14
+        { "id": 16, "name": "Vila do Conde", "lat": 41.3517, "lon": 8.7479, "alt": 150, "links": [] },                       // 15
+        { "id": 17, "name": "Vila Nova de Gaia", "lat": 41.1239, "lon": 8.6118, "alt": 500, "links": [] }
+      ]
+    }
+
+    for(const node of gData.nodes){
+      let cube = new THREE.Mesh(this.geometry, this.material);
+
+      cube.position.set(node.lat, node.lon, node.alt)
+      this.scene.add(cube);
+
+    }
+
     this.scene.add(this.cube);
 
     let aspectRatio = this.getAspectRatio();
@@ -50,7 +82,7 @@ export class RoadNetworkComponent implements OnInit, AfterViewInit {
       this.nearClippingPlane,
       this.farClippingPlane
     )
-    this.camera.position.z = this.cameraZ;
+    this.camera.position.z = 1000;
   }
 
   private getAspectRatio() {
@@ -74,6 +106,8 @@ export class RoadNetworkComponent implements OnInit, AfterViewInit {
       component.renderer.render(component.scene, component.camera);
     }())
   }
+
+
 
   constructor() { }
 
