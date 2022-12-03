269da6a5ef1dd9ee349ea68df955f075aea97a53
Gonçalo Boa-Nova <1190616@isep.ipp.pt>
2022-12-03 04:40:51 +0000

    LAPR5G033-20 Validations in Create Delivery Form

--- spa/src/app/create-delivery/create-delivery.component.ts
+++ spa/src/app/create-delivery/create-delivery.component.ts
@@ -18,6 +18,22 @@ export class CreateDeliveryComponent implements OnInit {
 
   onDeliveryCreate(deliveries: { dId: string, dDate: any, dWeight: number, dWarehouseId: string, putTime: number, takeTime: number }) {
 
+    const DATE_REGEX = new RegExp(/^(20[0-9]{2})[/]([1-9]|1[0-2])[/]([1-9]|[12][0-9]|3[01])$/);
+
+    if(!DATE_REGEX.test(deliveries.dDate.toString())){
+      alert("Invalid Date!");
+    }
+    else if(deliveries.dWeight < 0 || deliveries.dWeight == null){
+      alert("Invalid Weight!");
+    }
+    else if(deliveries.putTime < 0 || deliveries.putTime == null){
+      alert("Invalid Put Time!")
+    }
+    else if(deliveries.takeTime < 0 || deliveries.takeTime == null){
+      alert("Invalid Take Time!")
+    }
+    else alert("Delivery created with success!")
+
     const obj = {
       deliveryId: deliveries.dId, deliveryDate: deliveries.dDate, weight: deliveries.dWeight
       , warehouseId: deliveries.dWarehouseId, putDeliveryTime: deliveries.putTime,
